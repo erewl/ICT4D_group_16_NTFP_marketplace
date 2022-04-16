f39214dc05b149133f59216db3871732171b4b24
erewl <grunert.katrin@protonmail.com>
2022-04-16 18:14:22 +0200

    Add built frontend to backend

---
+++ foret-marche-backend/build/precache-manifest.6e470128008f0543e83c803a1944a5ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f925591c9d6903c44450dbf597a81cba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f7b30bc8b5d798302e",
+    "url": "/static/css/main.c80ad70b.chunk.css"
+  },
+  {
+    "revision": "e3f7b30bc8b5d798302e",
+    "url": "/static/js/main.325b418f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fd67fa4cc44e3306d66d38fb49fcf16f",
+    "url": "/static/media/logo.fd67fa4c.svg"
+  }
+]);
