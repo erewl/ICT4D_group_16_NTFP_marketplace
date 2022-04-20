3abd877e2eab10f335894b23abc21719d47496db
erewl <grunert.katrin@protonmail.com>
2022-04-20 16:48:56 +0200

    Add db connection

--- foret-marche-backend/main.py
+++ foret-marche-backend/main.py
@@ -2,8 +2,24 @@
    
 import os
 
-from flask import Flask, jsonify, request, render_template, Response
-from flask_cors import CORS
+from flask import Flask, request, render_template, Response
+from sqlalchemy import create_engine, text
+
+USER = os.getenv('DB_USER')
+PWD = os.getenv('DB_PWD')
+DATABASE = os.getenv('DATABASE')
+HOST = os.getenv('HOST')
+
+engine = create_engine(f'postgresql+psycopg2://{USER}:{PWD}@{HOST}/{DATABASE}')
+conn = engine.connect()
+
+query = text("SELECT * FROM users")
+results = engine.execute(query)
+  
+# View the records
+for record in results:
+    print("\n", record)
+
 
 app = Flask(__name__, static_url_path='/build/')
 # app = Flask(__name__)
