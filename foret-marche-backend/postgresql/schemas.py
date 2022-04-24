5cdf9d4f79b1a7b9d04fb2aea470a96fb711a5cc
erewl <grunert.katrin@protonmail.com>
2022-04-24 14:20:09 +0200

    Update schemas]

--- foret-marche-backend/postgresql/schemas.py
+++ foret-marche-backend/postgresql/schemas.py
@@ -6,21 +6,21 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = 'users'
-    user_id = Column(Sequence, primary_key=True)
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
     phone_number = Column(String(50), nullable=False)
 
 class Offers(Base):
     __tablename__ = 'offers'
-    offer_id = Column(Sequence, primary_key=True)
-    seller_id = Column(Sequence, ForeignKey(Users.user_id), primary_key=True)
+    offer_id = Column(Integer, primary_key=True, autoincrement=True)
+    seller_id = Column(Integer, ForeignKey(Users.user_id))
     product_name = Column(String(50), nullable=False)
     price = Column(Integer, nullable=False)
     quantity = Column(Integer, nullable=False)
 
 class Bids(Base):
     __tablename__ = 'bids'
-    bid_id = Column(Sequence, primary_key=True)
-    offer_id = Column(Sequence, ForeignKey(Offers.offer_id), primary_key=True)
-    seller_id = Column(Sequence, ForeignKey(Users.user_id), primary_key=True)
-    buyer_id = Column(Sequence, ForeignKey(Users.user_id), primary_key=True)
+    bid_id = Column(Integer, primary_key=True, autoincrement=True)
+    offer_id = Column(Sequence, ForeignKey(Offers.offer_id))
+    seller_id = Column(Sequence, ForeignKey(Users.user_id))
+    buyer_id = Column(Sequence, ForeignKey(Users.user_id))
     quantity = Column(Integer, nullable=False)
